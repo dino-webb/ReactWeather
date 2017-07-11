cb353bd2bb774de6ba95dc99313405f7fd6e0245
Dino Adani <dino.adani@crowdstar.com>
2017-07-10 22:50:58 -0700

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  entry: './app/app.jsx',
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  resolve: {
+    root: __dirname,
+    alias: {
+      Main: 'app/components/Main',
+      Nav: 'app/components/Nav',
+      Weather: 'app/components/Weather',
+      WeatherForm: 'app/components/WeatherForm',
+      WeatherMessage: 'app/components/WeatherMessage',
+      About: 'app/components/About',
+      Examples: 'app/components/Examples',
+      openWeatherMap: 'app/api/openWeatherMap'
+    },
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0']
+        },
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/
+      }
+    ]
+  },
+  devtool: 'cheap-module-eval-source-map'
+};
